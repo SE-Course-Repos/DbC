c93e7d8a175ceaf2436530a76489893b925a88ca
wggster <wgg@cs.ucsd.edu>
2020-02-06 22:05:07 -0800

    Catch exception in Main/client test to go green.

--- src/test/java/edu/cs/ucsd/dbc/MainTest.java
+++ src/test/java/edu/cs/ucsd/dbc/MainTest.java
@@ -1,10 +1,11 @@
 package edu.cs.ucsd.dbc;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MainTest {
     @Test
     void TestMain() {
-        Main.main(new String[]{"ignored"});
+        assertThrows(Contract.ViolationException.class, ()-> Main.main(new String[]{"ignored"}));
     }
 }
